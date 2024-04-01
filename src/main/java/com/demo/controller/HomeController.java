1d7d76b079a941a1c927488351852f6b1e00d9b9
Hadyan Yuma Ekdiattan <hadyanyuma.sekawanmedia@gmail.com>
2024-04-01 08:12:12 +0700

    learn

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,7 @@ public class HomeController extends Controller
     public String home() 
     {
         String resultFromOtherClass = homeService.someMethod();
-        return this.successResponse(resultFromOtherClass);
+        return resultFromOtherClass;
     }
 }
 
